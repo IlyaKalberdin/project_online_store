2374886fa1679d61a6a72992ec85dedca8c79500
Ilya Kalberdin <ilyakalberdin@gmail.com>
2023-12-30 07:22:33 +0300

    Написан код для получения контактной информации

--- catalog_app/views.py
+++ catalog_app/views.py
@@ -8,4 +8,9 @@ def home_page(request):
 
 def contact_page(request):
     # Функция возвращает страницу contact_page.html
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        number = request.POST.get('number')
+
+        print(f'{name}, {number}')
     return render(request, 'catalog_app/contact_page.html')
