928d73d3b3345296c20669022bc79a0b2128ad1c
Ilya Kalberdin <ilyakalberdin@gmail.com>
2023-12-30 07:13:04 +0300

    Написан код для отображения страниц сайта

--- catalog_app/views.py
+++ catalog_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page(request):
+    # Функция возвращает страницу home_page.html
+    return render(request, 'catalog_app/home_page.html')
+
+
+def contact_page(request):
+    # Функция возвращает страницу contact_page.html
+    return render(request, 'catalog_app/contact_page.html')
--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog_app.urls', namespace='catalog_app'))
+    path('', include('catalog_app.urls'))
 ]
