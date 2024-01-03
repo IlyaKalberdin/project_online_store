544d1fa8c092a834b50c5b05a40c3abcee936b81
Ilya Kalberdin <ilyakalberdin@gmail.com>
2024-01-03 19:52:24 +0300

    Написаны модели продукт и категория

--- catalog_app/models.py
+++ catalog_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='product_image/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за штуку')
+    creation_date = models.DateField(verbose_name='дата создания')
+    last_modified_date = models.DateField(verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return (f'{self.name} {self.category} {self.price} '
+                f'{self.creation_date} {self.last_modified_date} {self.description}')
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
