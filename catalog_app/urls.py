2c9e629b00157fc8207e63265bd2118c9ed54164
Ilya Kalberdin <ilyakalberdin@gmail.com>
2024-01-15 18:38:39 +0300

    Добавлен путь для страницы продукта

--- catalog_app/urls.py
+++ catalog_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from catalog_app.views import home_page, contact_page
+from catalog_app.views import home_page, contact_page, product_page
 
 urlpatterns = [
     path('', home_page),
-    path('contacts', contact_page)
+    path('contacts', contact_page),
+    path('product/<int:product_id>', product_page)
 ]
