b22c55898bd5ee0ea93113ddc23562980de7bfe1
Ilya Kalberdin <ilyakalberdin@gmail.com>
2024-01-15 20:28:19 +0300

    Переписаны ссылки на страницы с помощью тега url

--- catalog_app/urls.py
+++ catalog_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from catalog_app.views import home_page, contact_page, product_page
+from catalog_app.apps import MainappConfig
+
+app_name = MainappConfig.name
 
 urlpatterns = [
-    path('', home_page),
-    path('contacts', contact_page),
-    path('product/<int:product_id>', product_page)
+    path('', home_page, name='home'),
+    path('contacts', contact_page, name='contacts'),
+    path('product/<int:product_id>', product_page, name='product')
 ]
