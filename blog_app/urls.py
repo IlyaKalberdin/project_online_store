d262c11373925b6413e9619e4a5dc385702b91d4
Ilya Kalberdin <ilyakalberdin@gmail.com>
2024-01-28 00:27:19 +0300

    Добавлен файл для url-ов

---
+++ blog_app/urls.py
@@ -0,0 +1,52 @@
+from django.urls import path
+from blog_app.apps import BlogAppConfig
+
+app_name = BlogAppConfig.name
+
+urlpatterns = [
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
