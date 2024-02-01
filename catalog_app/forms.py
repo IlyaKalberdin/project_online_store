4343d0435a6729c6c11bb0c175faa8ac8252d22d
Ilya Kalberdin <ilyakalberdin@gmail.com>
2024-02-01 22:31:35 +0300

    Добавлена валидация названия и описания продукта по словам исключениям

--- catalog_app/forms.py
+++ catalog_app/forms.py
@@ -14,3 +14,42 @@ class ProductForm(forms.ModelForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
 
+    def clean_name(self):
+        cleaned_name = self.cleaned_data.get('name')
+
+        name_exception_words = self.get_clean_set(cleaned_name)
+
+        if name_exception_words:
+            name_exception_words = ', '.join(name_exception_words)
+            raise forms.ValidationError(f'Нельзя использовать следущие слова: {name_exception_words}')
+
+        return cleaned_name
+
+    def clean_description(self):
+        cleaned_description = self.cleaned_data.get('description')
+
+        description_exception_words = self.get_clean_set(cleaned_description)
+
+        if description_exception_words:
+            description_exception_words = ', '.join(description_exception_words)
+            raise forms.ValidationError(f'Нельзя использовать следущие слова: {description_exception_words}')
+
+        return cleaned_description
+
+    @staticmethod
+    def get_clean_set(string):
+        """Метод принимает строку и возвращает множество со словами исключениями
+        из этой строки"""
+        translate_dict = {ord(','): None, ord('-'): None, ord(':'): None,
+                          ord(';'): None, ord('.'): None}
+
+        exception_words = {'казино', 'криптовалюта', 'крипта', 'биржа', 'дешево',
+                           'бесплатно', 'обман', 'полиция', 'радар'}
+
+        string = string.lower()
+        string = string.translate(translate_dict)
+        string_set = set(string.split())
+
+        string_exception_words = exception_words.intersection(string_set)
+
+        return string_exception_words
