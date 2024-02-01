de0aa0b979b68f6f8b01090454275888da788fa1
Ilya Kalberdin <ilyakalberdin@gmail.com>
2024-02-01 19:45:40 +0300

    Добавлена стилизация формы

--- catalog_app/forms.py
+++ catalog_app/forms.py
@@ -7,3 +7,10 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ('category', 'price', 'name', 'description', 'image')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
