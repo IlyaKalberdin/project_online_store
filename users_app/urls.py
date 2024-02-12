af5b721ef8d2c1ef848207011dad693c727585a8
Ilya Kalberdin <ilyakalberdin@gmail.com>
2024-02-12 19:40:29 +0300

    Добавлен контроллер и урл для регистрации пользователя

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,9 +1,12 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from users_app.views import RegistrationView
 from django.urls import path
 from users_app.apps import UsersAppConfig
 
 app_name = UsersAppConfig.name
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(template_name='users_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('registration/', RegistrationView.as_view(), name='registration')
     ]
--- users_app/views.py
+++ users_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from users_app.forms import UserRegistrationForm
+from users_app.models import User
 
-# Create your views here.
+
+class RegistrationView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users_app/registration.html'
+    success_url = reverse_lazy('users_app:login')
+
