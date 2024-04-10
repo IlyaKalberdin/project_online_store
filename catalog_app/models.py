9ad5e8b827576bad88e09266eda008eda26c9b91
Ilya Kalberdin <ilyakalberdin@gmail.com>
2024-04-10 20:46:13 +0300

    Добавлен признак публикации

--- catalog_app/models.py
+++ catalog_app/models.py
@@ -15,6 +15,7 @@ class Product(models.Model):
     creation_date = models.DateField(verbose_name='дата создания')
     last_modified_date = models.DateField(verbose_name='дата последнего изменения')
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='автор')
+    is_published = models.BooleanField(default=False, verbose_name='опубликовано ли')
 
     def __str__(self):
         # Строковое отображение объекта
